6101c59c67b87219ef56cbb47bdc3431fe859659
islamnurdinov <nurdinovislam4@gmail.com>
2024-12-04 19:42:27 +0600

    add

--- config/urls.py
+++ config/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
     path('api/v1/users/', include('users.urls')),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
