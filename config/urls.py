832b8cf3df2b736fb0787e5d6714165203c2c053
islamnurdinov <nurdinovislam4@gmail.com>
2024-12-04 23:44:34 +0600

    add

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,33 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework import permissions
+
 from config import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for my project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myproject.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),  # Установите разрешение на AllowAny
+
+)
 
 urlpatterns = [
     # path('api-token-auth/', auth_views.obtain_auth_token),
     path("admin/", admin.site.urls),
     path('api/v1/products/', include('products.urls')),
     path('api/v1/users/', include('users.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
